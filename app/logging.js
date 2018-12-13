dc9adfe260272c821fd08fec01e092edd9c211ee
Nessmonk <zveau@justbetter.nl>
2018-12-13 16:46:59 +0100

    Guild id added to log

--- app/logging.js
+++ app/logging.js
@@ -2,14 +2,14 @@ function logging(bot, logger) {
     bot.on('guildMemberAdd', member => {
         const channel = member.guild.channels.find(ch => ch.name == 'log');
         if (!channel) return;
-        logger.info(`${member} joined the server.`)
+        logger.info(`${member} joined ` + member.guild + ". Guild id is " + member.guild.id);
         channel.send(`Welcome to the server, ${member}`);
     });
     
     bot.on('guildMemberRemove', member => {
         const channel = member.guild.channels.find(ch => ch.name == 'log');
         if (!channel) return;
-        logger.info(`${member} left the server.`)
+        logger.info(`${member} left ` + member.guild + ". Guild id is " + member.guild.id);
         channel.send(`Goodbye, ${member}`);
     });
 }
