9562a4ac4147b16a6be35d8749138fe765892088
Ness <thetosti55@gmail.com>
2019-12-13 15:40:39 +0100

    Longer log file

--- commands/callLog.js
+++ commands/callLog.js
@@ -1,5 +1,5 @@
 function callLog(message, logger, readLastLines) {
-    readLastLines.read('./app/logfile.log', 5).then((lines) => message.channel.send('```'+lines+'```'));
+    readLastLines.read('./app/logfile.log', 10).then((lines) => message.channel.send('```'+lines+'```'));
 
     if (message.channel.type !== "dm") {
         logger.info(message.author.username + '#' + message.author.discriminator + ' requested logs in ' + message.guild.name); 
